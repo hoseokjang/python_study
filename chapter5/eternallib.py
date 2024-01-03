205e2f12b82b3ccb64211b74dbb51d2e969e8f67
hoseokjang <hoseok210@gmail.com>
2024-01-03 17:09:13 +0900

    pip 스터디

---
+++ chapter5/eternallib.py
@@ -0,0 +1,17 @@
+# 1. pip
+# 모듈이나 패키지 설치할때 pip 사용
+
+# pip install (최신 버전)
+# pip install SomePackage
+
+# pip uninstall
+# pip uninstall SomePackage
+
+# 특정 버전으로 설치
+# pip install SomePackage==version
+
+# 최신 버전으로 업그레이드
+# pip install --upgrade SomePackage
+
+# 설치된 패키지 확인
+# pip list
